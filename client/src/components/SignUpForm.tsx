ebfbab961faa0846c03fe344fc610e1b909caf4c
elperez750 <elperez750@gmail.com>
2025-01-16 18:49:35 -0800

    changed url

--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { BASE_URL } from "../api/services";
+import { BASE_URL } from "../api/services"
 
 const SignUpForm: React.FC = () => {
   const navigate = useNavigate();
