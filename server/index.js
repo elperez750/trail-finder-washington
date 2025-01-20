453586a73703d9f1b649e52ce59dba3122d288d9
elperez750 <elperez750@gmail.com>
2025-01-20 14:43:40 -0800

    fin

--- server/index.js
+++ server/index.js
@@ -34,14 +34,6 @@ app.use(cors({
     credentials: true, // Allow cookies/credentials
 }));
 
-// Manually set headers (if needed)
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://trail-finder-washington-client.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 
 // Middleware
 app.use(express.json());
