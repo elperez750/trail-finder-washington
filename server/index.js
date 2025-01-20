21345159717bf52add4b2bc623c853f3a10f9fd9
elperez750 <elperez750@gmail.com>
2025-01-20 14:44:47 -0800

    f

--- server/index.js
+++ server/index.js
@@ -14,25 +14,7 @@ connectDB();
 
 
 //Cors setup
-const allowedOrigins = [
-    'http://localhost:5173', // Local development
-    'https://trail-finder-washington-client-554bnl8xf-elperez750s-projects.vercel.app', // Deployed frontend
-    'https://trail-finder-washington-client.vercel.app',
-    'https://trail-finder-washington-client-5lyaeutap-elperez750s-projects.vercel.app',
-];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true); // Allow request
-        } else {
-            console.error(`Blocked by CORS: ${origin}`); // Debug blocked origins
-            callback(new Error('Not allowed by CORS')); // Block request
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // Allow cookies/credentials
-}));
+app.use(cors());
 
 
 // Middleware
