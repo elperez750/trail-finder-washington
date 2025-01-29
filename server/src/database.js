de520186753dd9f5f864c1116a6466708ba8d887
elperez750 <elperez750@gmail.com>
2025-01-28 22:07:21 -0800

    getting closer

--- server/src/database.js
+++ server/src/database.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+
+
+    const MONGO_URI = process.env.NODE_ENV === "production" ?
+    process.env.MONGO_URI : process.env.MONGO_URI_LOCAL;
+
+    console.log(MONGO_URI);
     try {
         console.log("🚀 Connecting to MongoDB...");
-        console.log(process.env.MONGO_URI);
-        await mongoose.connect(process.env.MONGO_URI, {
+        console.log(MONGO_URI);
+        await mongoose.connect(MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
+
+
+        console.log("✅ Connected to Database:", mongoose.connection.name);
+
         console.log("✅ MongoDB Connected Successfully");
     } catch (error) {
         console.error("❌ MongoDB Connection Failed:", error.message);
